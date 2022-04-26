496d2ef7546d3b1bd4f206ce137557fce2ba6445
Fabian <f.stiehle@gmail.com>
2022-04-26 18:36:43 +0200

    add figure export

--- plot.R
+++ plot.R
@@ -8,7 +8,7 @@ library(ggplot2)
 library(scales)
 library(stringr)
 
-data <- read_csv("Projects/RA_SLR/Literature_review_collection.xlsx - DataExtraction.csv", 
+data <- read_csv("Literature_review_collection.xlsx - DataExtraction.csv", 
   col_types = cols(Title = col_character(), 
   Year = col_date(format = "%Y")),
   skip = 2)
@@ -23,15 +23,14 @@ data <- data %>%
 by_country <- data %>%
   group_by(Country) %>%
   summarise(n=n()) %>%
-  rev(arrange(n, Country))
+  arrange(n, desc(Country))
 
 other <- by_country %>%
   filter(n<=1)
 by_country <- by_country %>%
   filter(! Country %in% other$Country)
   
-by_country <- rbind(data.frame(Country="Other (<2)", n=count(other)), by_country)
-
+rbind(by_country, data.frame(Country="Other (<2)", n=count(other)))
 by_country$ID <- seq.int(nrow(by_country))
 by_country$Country <- factor(by_country$ID, 
                              labels=by_country$Country,
@@ -43,14 +42,16 @@ ggplot(by_country, aes(x=n, y=Country)) +
   ylab("Country") +
   xlab("Number of Publications") + 
   scale_x_continuous(n.breaks=max(by_country$n), minor_breaks = NULL)
+ggsave("countries.PDF", width = 4, height = 2)
 
 # By year
 ggplot(data, aes(x=Year)) +
   geom_bar(fill=colors[1]) +
   ylab("Publication Year") +
   xlab("Number of Publications") + 
-  scale_x_date(date_labels = "%Y", breaks = "2 years", minor_breaks = NULL) +
+  scale_x_date(date_labels = "%Y", breaks = "3 years", minor_breaks = NULL) +
   scale_y_continuous(n.breaks=max(by_country$n), minor_breaks = NULL)
+ggsave("years.PDF", width = 4, height = 2)
 
 # Optimization goals
 goals <- data %>%
@@ -69,13 +70,15 @@ ggplot(goals, aes(y=Goal, x=n, fill=`Goal Type`)) +
   scale_fill_manual(name = element_blank(), values = colors) +
   scale_x_continuous(breaks=pretty_breaks(n=floor(max(goals$n)/2)), minor_breaks = NULL) +
   xlab("Number of Publications")
+ggsave("goals.PDF", width = 6, height = 2)
 
 # Allocation Capabilities
 ggplot(data, aes(x=`Allocation capability`)) +
   geom_bar(fill=colors[4]) +
   ylab("Number of Publications") + 
   scale_y_continuous(breaks=pretty_breaks(n=7), minor_breaks = NULL) + 
-  geom_text(aes(label = ..count..), stat = "count", vjust = 2, colour = "white")
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")
+ggsave("allocation_small.PDF", width = 3, height = 2)
 
 # Solution techniques grouped by their allocation goal
 sol <- select(data, Goal, `Resource allocation technique type`)
@@ -100,6 +103,7 @@ ggplot(sol, aes(y=Goal, fill=t)) +
   scale_fill_manual(name = element_blank(), values = colors) +
   scale_x_continuous(breaks=pretty_breaks(n=floor(max(sol$n)/2)), minor_breaks = NULL) +
   xlab("Number of Publications")
+ggsave("solution_technique.PDF", width = 8, height = 2)
 
 # Maturity levels
 # we do this for ordering
@@ -117,17 +121,20 @@ mat$`Evaluation Type` <- factor(mat$ID,
 
 ggplot(mat, aes(y=`Evaluation Type`, fill=`Prototypical implementation type`)) +
   geom_bar() +
-  scale_fill_manual(name = element_blank(), values = c("available"= colors_rating[5],
+  scale_fill_manual(name = "Prototype", values = c("available"= colors_rating[5],
                                                        "not acc., pseudoc." = colors_rating[4],
                                                        "pseudocode" = colors_rating[3],
                                                        "not accessible" = colors_rating[2],
                                                        "no" = colors_rating[1])) +
   scale_x_continuous(breaks=pretty_breaks(n=floor(max(mat$n)/2)), minor_breaks = NULL) +
   xlab("Number of Publications")
+ggsave("evaluation.PDF", width = 8, height = 2)
 
 ggplot(data, aes(x=str_to_title(Maturity), fill=str_to_title(Maturity))) +
   geom_bar() +
   scale_fill_manual(name = element_blank(), values = rev(tail(colors_rating, -1))) +
-  xlab("Number of Publications") +
+  xlab("Maturity Level") +
+  ylab("Number of Publications") +
   theme(legend.position="none") + 
-  geom_text(aes(label = ..count..), stat = "count", vjust = 2, colour = "black")
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "#353839")
+ggsave("maturity.PDF", width = 3, height = 2)
